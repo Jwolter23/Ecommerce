ae38ed5febe41d5ee34ebde2fbf72382750b6229
Jordan Wolter <jwolter456@gmail.com>
2023-04-25 15:01:34 -0400

    nav elements set up

--- ecom_store/src/components/Nav.jsx
+++ ecom_store/src/components/Nav.jsx
@@ -1,6 +1,13 @@
+import { Link } from 'react-router-dom'
 export default function Nav () {
 
     return (
-        <h3>Nav Bar here</h3>
+        <div className="navBar">
+            <Link to="/" className='links'> Home </Link>
+            <Link to="/men" className='links'> Men </Link>
+            <Link to="/women" className='links'> Women </Link>
+            <Link to="/account" className='links'> Account </Link>
+
+        </div>
     )
 }
